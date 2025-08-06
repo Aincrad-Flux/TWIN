1978a5c6d63947947619dece880da3901aa81258
Thibault Pouch <thibault.pouch@epitech.eu>
2025-08-06 13:58:41 +0200

    [Feat] Add initial middleware for API key validation

---
+++ src/middleware/key.js
@@ -0,0 +1,10 @@
+/*
+ * T.W.I.N - Ticket Workflow Integration Network
+ *
+ *   Author : Pandor
+ *   Organization : Aincrad-Flux
+ *
+ *  filename : src/middleware/key.js
+ *  description : Middleware for handling API key validation
+*/
+
