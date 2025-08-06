5a589352dd20a497a63a393fcf787fbe860f1c47
Thibault Pouch <thibault.pouch@epitech.eu>
2025-08-06 16:19:56 +0200

    [Feat] Create DataManager, Issue, and Comment classes for database interactions

--- src/utils/dataManager.js
+++ src/utils/dataManager.js
@@ -12,6 +12,7 @@ const fs = require('fs');
 const path = require('path');
 const logger = require('../config/logger');
 
+//* ------------ Main Data Manager ------------
 class DataManager {
   constructor(dbPath) {
     this.dbPath = dbPath;
@@ -25,4 +26,39 @@ class DataManager {
    *  - Comments
    *  - LogActions
    */
+}
+
+//* ------------ Issue Data Manager ------------
+class Issue extends DataManager {
+  constructor(issueData) {
+    super();
+    this.id = issueData.id;
+    this.key = issueData.key;
+    this.summary = issueData.summary;
+    this.description = issueData.description;
+    this.status = issueData.status;
+    this.assignee = issueData.assignee;
+    this.createdAt = new Date(issueData.createdAt);
+    this.updatedAt = new Date(issueData.updatedAt);
+  }
+
+  static fromJSON(jsonData) {
+    return new Issue(jsonData);
+  }
+}
+
+//* ------------ Comment Data Manager ------------
+class Comment extends DataManager {
+    constructor(commentData) {
+        super();
+        this.id = commentData.id;
+        this.issueId = commentData.issueId;
+        this.author = commentData.author;
+        this.content = commentData.content;
+        this.createdAt = new Date(commentData.createdAt);
+    }
+
+    static fromJSON(jsonData) {
+        return new Comment(jsonData);
+    }
 }
